35bdd24a2332a1d9271acac9c33ec9fe79f7fe3b
Santiago Torres <santorres724@gmail.com>
2024-06-16 01:33:31 -0300

    CRUD de Categoria

---
+++ SistemaInventario.Modelos/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El Nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "El Nombre debe contener maximo 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La Descripcion es requerida")]
+        [MaxLength(100, ErrorMessage = "La descripcion debe contener maximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El Estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}

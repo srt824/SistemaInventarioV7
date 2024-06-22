c6d01579f89927c1944c3ea8a5631d1728ef1ea7
Santiago Torres <santorres724@gmail.com>
2024-06-22 03:05:40 -0300

    CRUD de Producto

---
+++ SistemaInventario.Modelos/Producto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El Numero de Serie es requerido")]
+        [MaxLength(60)]
+        public string NumeroSerie { get; set; }
+
+        [Required(ErrorMessage = "La Descripcion es requerida")]
+        [MaxLength(60)]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El Precio es requerido")]
+        public double Precio { get; set; }
+
+        [Required(ErrorMessage = "El Costo es requerido")]
+        public double Costo { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+        [Required(ErrorMessage = "El Estado es requerido")]
+        public bool Estado { get; set; }
+
+        [Required(ErrorMessage = "El Id de Categoria es requerido")]
+        public int CategoriaId { get; set; }
+
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+
+        [Required(ErrorMessage = "El Id de Marca es requerido")]
+        public int MarcaId { get; set; }
+
+        [ForeignKey("MarcaId")]
+        public Marca Marca { get; set; }
+
+        public int? PadreId { get; set; }
+        public virtual Producto Padre { get; set; }
+    }
+}

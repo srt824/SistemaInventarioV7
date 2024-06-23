32fd5ec99e3961fa486ec13b6197be9fcd22ac0c
Santiago Torres <santorres724@gmail.com>
2024-06-23 03:07:57 -0300

    Paginacion

--- SistemaInventario.Modelos/ErrorViewModel.cs
+++ SistemaInventario.Modelos/ErrorViewModel.cs
@@ -1,8 +1,8 @@
-namespace SistemaInventario.Modelos.ViewModels
+namespace SistemaInventario.Modelos
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
